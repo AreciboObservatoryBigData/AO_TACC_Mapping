c7fda9c022f4d6837230098a2e0af2a510cdfacf
ftorres-ucf <francisco.torres@ucf.edu>
2023-04-19 12:14:49 -0400

    Simplified DB and changed dir_listing to accomodate the changes

--- dir_listing/listing.sh
+++ dir_listing/listing.sh
@@ -25,56 +25,35 @@ if [ -f "$OUTPUT" ]; then
     rm $OUTPUT
 fi
 
-# Create output directory
-# mkdir -p $OUTPUT
-
-# get basename of directory
-# DIR_BASENAME=$(basename $OUTPUT)
-
-
-# three files will be made, with the output_directory specified
-# files_output="$OUTPUT/$DIR_BASENAME"_files.tsv
-# dirs_output="$OUTPUT/$DIR_BASENAME"_dirs.tsv
-# symlinks_output="$OUTPUT/$DIR_BASENAME"_symlinks.tsv
-
-# echo $files_output
-# echo $dirs_output
-# echo $symlinks_output
 
 
 echo "Starting Directory: $DIR"
 
 # Log start time, date, and final command
 echo "Start time: $(date)" >> run_log.log
-echo "Command: find $DIR -type f -printf %f\t%p\t%y\t%s\t%A@\t%T@\t%C@\n >> $files_output" >> run_log.log
+echo "Command: find $DIR -type f -printf %f\t%p\t%y\t%s\t%A@\t%T@\t%C@\n >> $OUTPUT" >> run_log.log
 
 # Create output tsv file
 touch $OUTPUT
 
 # Add header to tsv file
-echo -e "fileName\tfilePath\tfileType\tfileSize\tfileAtime\tfileMtime\tfileCtime" >> $OUTPUT
+echo -e "fileName\tfilePath\tfileType\tfileSize\tfileAtime\tfileMtime\tfileCtime\tpoints_to" >> $OUTPUT
 
 # Use:
 # find {dir_path} -type f -exec stat --printf='\"%n\"\t%s\t%W\t%X\t%Y\t%Z\n' {} + >> {results_path}
 # to get the file name, size, last access time, last modification time, last status change time, and creation time
-find $DIR -type f -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\n" >> $files_output
+find $DIR -type f -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t\n" >> $OUTPUT
 
 echo "Finished files in Directory: $DIR"
 
 # Log start time, date, and final command
 echo "Start time: $(date)" >> run_log.log
-echo "Command: find $DIR -type d -printf %f\t%p\t%y\t%s\t%A@\t%T@\t%C@\n >> $dirs_output" >> run_log.log
-
-# Create output tsv file
-touch $dirs_output
-
-# Add header to tsv file
-echo -e "fileName\tfilePath\tfileType\tfileSize\tfileAtime\tfileMtime\tfileCtime" >> $dirs_output
+echo "Command: find $DIR -type d -printf %f\t%p\t%y\t%s\t%A@\t%T@\t%C@\n >> $OUTPUT" >> run_log.log
 
 # Use:
 # find {dir_path} -type f -exec stat --printf='\"%n\"\t%s\t%W\t%X\t%Y\t%Z\n' {} + >> {results_path}
 # to get the file name, size, last access time, last modification time, last status change time, and creation time
-find $DIR -type d -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\n" >> $dirs_output
+find $DIR -type d -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t\n" >> $OUTPUT
 
 echo "Finished directories in Directory: $DIR"
 
@@ -82,16 +61,11 @@ echo "Finished directories in Directory: $DIR"
 echo "Start time: $(date)" >> run_log.log
 echo "Command: find -type l -printf %f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t -exec sh -c 'readlink -f \"\$0\"' {} \; -printf \n" >> run_log.log
 
-# Create output tsv file
-touch $symlinks_output
-
-# Add header to tsv file
-echo -e "fileName\tfilePath\tfileType\tfileSize\tfileAtime\tfileMtime\tfileCtime\tpoints_to" >> $symlinks_output
 
 # Use:
 # find {dir_path} -type f -exec stat --printf='\"%n\"\t%s\t%W\t%X\t%Y\t%Z\n' {} + >> {results_path}
 # to get the file name, size, last access time, last modification time, last status change time, and creation time
-find $DIR -type l -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t" -exec sh -c 'readlink -f "$0"' {} \; -printf "\n" >> $symlinks_output
+find $DIR -type l -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t" -exec bash -c 'readlink "$0"' {} \; >> $OUTPUT
 
 echo "Finished symlinks in Directory: $DIR"
 
