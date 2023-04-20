c09b8493f2947f97baf44fdbc241e2972643a86d
ftorres-ucf <francisco.torres@ucf.edu>
2023-04-19 20:41:16 -0400

    Changed find command to run only once

--- dir_listing/listing.sh
+++ dir_listing/listing.sh
@@ -40,14 +40,23 @@ touch $OUTPUT
 echo -e "fileName\tfilePath\tfileType\tfileSize\tfileAtime\tfileMtime\tfileCtime\tpoints_to" >> $OUTPUT
 
 
+
+# Log start time, date, and final command
+echo "Start time: $(date)" >> run_log.log
+echo "Command: find $OUTPUT -printf %f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t -exec sh -c 'readlink -f \"\$0\"' {} \; -printf \n" >> run_log.log
+
+
 # Use:
 # find {dir_path} -type f -exec stat --printf='\"%n\"\t%s\t%W\t%X\t%Y\t%Z\n' {} + >> {results_path}
 # to get the file name, size, last access time, last modification time, last status change time, and creation time
-find $DIR -type l -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t" -exec bash -c 'readlink "$0"' {} \; >> $OUTPUT
+find $DIR -printf "%f\t%p\t%y\t%s\t%A@\t%T@\t%C@\t" -exec bash -c 'readlink "$0" 2>error.log || echo -n' {} \; -printf "\n" | sed '/^$/d' >> $OUTPUT
+
+
+
+
 
 echo "Finished Directory: $DIR"
 
-echo "Finished Directory: $DIR"
 
 # Log end time and date
 echo "End time: $(date)" >> run_log.log
